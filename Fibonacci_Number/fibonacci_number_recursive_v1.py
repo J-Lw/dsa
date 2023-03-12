887487fcd41dbbb7b8af516a33615129762a4d7f
J-Lw <John@DeepIntel.us>
2023-03-12 19:46:03 -0300

    Updated recursive algorithm files

---
+++ Fibonacci_Number/fibonacci_number_recursive_v1.py
@@ -0,0 +1,12 @@
+#Recursive fibonacci algorithm using two variables
+
+_MAX_N = 100
+_MIN_N = 1
+
+def derive_fibonacci_number(n: int) -> int:
+   if n > _MAX_N or n < _MIN_N:
+        return -1
+   if n <= 2:
+        return 1
+   if n > 2:
+      return derive_fibonacci_number(n-1) + derive_fibonacci_number(n-2)
--- Fibonacci_Number/test_recursive_fibonacci.py
+++ Fibonacci_Number/test_recursive_fibonacci.py
@@ -1,5 +1,5 @@
 import unittest
-import fibonacci_number_recursive_v1
+from fibonacci_number_recursive_v1 import derive_fibonacci_number
 
 class TestRecursiveFibonacci(unittest.TestCase):
     def test_derive_first_fn(self):
@@ -17,3 +17,6 @@ class TestRecursiveFibonacci(unittest.TestCase):
     def test_derive_nineteenth_fn(self):
         product = derive_fibonacci_number(19)
         self.assertEqual(product, 4181)
+
+if __name__ == '__main__':
+    unittest.main()
